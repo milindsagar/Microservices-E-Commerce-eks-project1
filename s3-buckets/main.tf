f425ac20de092fc4c309066562aeb915c7567702
milindsagar <119028898+milindsagar@users.noreply.github.com>
2026-08-11 16:10:21 +0530

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "sagardawkhar1"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "sagardawkhar1"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "sagardawkhar2"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "sagardawkhar2"
     Environment = "dev"
   }
 }
